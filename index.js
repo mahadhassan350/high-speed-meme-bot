4dcf355c4a9e4781a2d2fc68f62bc1db5be8f34d
mahadhassan350 <mahadabdulahi0013@gmail.com>
2024-12-24 00:06:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 {
   "name": "high-speed-meme-bot",
   "version": "1.0.0",
+  "description": "A bot for high-speed trading on Solana.",
   "main": "index.js",
   "scripts": {
     "start": "node index.js"
   },
+  "keywords": ["solana", "trading", "bot"],
+  "author": "Mahad",
+  "license": "ISC",
   "dependencies": {
     "@solana/web3.js": "^1.76.0",
     "helius-sdk": "^1.0.15"
